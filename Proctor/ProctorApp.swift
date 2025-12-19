e9f221c1eecf27b6b8e689e7315c0f70aacec224
Shamil <shamilgaliev18mail.ru@MacBook-Pro-Shamil.local>
2025-12-19 17:19:58 +0300

    init

---
+++ Proctor/ProctorApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProctorApp.swift
+//  Proctor
+//
+//  Created by Shamil on 19.12.2025.
+//
+
+import SwiftUI
+
+@main
+struct ProctorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ProctorTests/ProctorTests.swift
@@ -0,0 +1,17 @@
+//
+//  ProctorTests.swift
+//  ProctorTests
+//
+//  Created by Shamil on 19.12.2025.
+//
+
+import Testing
+@testable import Proctor
+
+struct ProctorTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
